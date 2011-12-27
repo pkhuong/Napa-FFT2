4c4a74a46c7d946b5e7105bc1499a21b00948ad7
Paul Khuong <pvk@pvk.ca>
2011-12-27 13:21:35 -0500

    Twiddle while transposing while FFTing

--- medium-fft.lisp
+++ medium-fft.lisp
@@ -5,17 +5,23 @@
   (declare (type function small-fft)
            (type size size)
            (type complex-sample-array vec dst tmp
-                 twiddle ck-factors))
+                 twiddle ck-factors)
+           (optimize speed (safety 0)))
   (let ((half-size (ash 1 (truncate (integer-length (1- size))
                                     2))))
-    (loop for i of-type index below half-size do
-      (loop for j of-type index below half-size
-            for k of-type index from i by half-size
-            do (setf (ref tmp j) (ref vec k)))
-      (funcall small-fft
-               dst tmp twiddle
-               (* i half-size)   0))
-    (map-into dst #'* dst ck-factors)
+    (loop for i of-type index below half-size
+          for dsts of-type index by half-size
+          do (loop for j of-type index below half-size
+                   for k of-type index from i by half-size
+                   do (setf (ref tmp j) (ref vec k)))
+             (funcall small-fft
+                      dst  tmp twiddle
+                      dsts 0)
+             (loop for i from dsts
+                   for count from half-size above 0
+                   do (setf (ref dst i)
+                            (* (ref dst i)
+                               (ref ck-factors i)))))
     (loop for i of-type index below half-size do
       (loop for j of-type index below half-size
             for k of-type index from i by half-size
