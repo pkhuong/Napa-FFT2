7891c52abc5b66d6e6c18106d16ab89f13840d76
Paul Khuong <pvk@pvk.ca>
2011-12-27 12:34:40 -0500

    Must go through setf expander, otherwise index computation is opaque to setf-ref

--- indexing.lisp
+++ indexing.lisp
@@ -2,12 +2,15 @@
 ;;;
 ;;; Only handles simple expression involving addition and multiplication
 ;;; by constants.
+;;;
+;;; Nothing SBCL-specific in the codegen (e.g. %lea), but it does use
+;;; Python's semi-internal constant-form-value.
 
-(declaim (inline ref (setf ref)))
+(declaim (inline ref %set))
 (defun ref (array index)
   (aref array index))
 
-(defun (setf ref) (value array index)
+(defun %set (array index value)
   (setf (aref array index) value))
 
 (defvar *current-multiplier*)
@@ -60,7 +63,32 @@
            (first addends))
           (t 0))))
 
+
+;; Sorry.
+;; I could break it up, but I'd have to pass terms/noise/offset around.
+;; Maybe if the function grows
+;;
+;; I like to think of it as CPSing the index computation, with specialised
+;; continuations of the type (+ (* k [hole]) ...): the result of each 
+;; expression will be multiplied by *current-multiplier* and added to the
+;; final index value.
+;;
+;; *current-multiplier* is the ``k'' by which the expression to
+;; evaluate will be multiplied.  The accumulator is represented by
+;; three terms: offset is a bunch of purely constant values, noise
+;; values that should simply be added, and terms is a table of
+;; multiplier -> multiplied values.
+;;
+;; A lot of this is also handled in most compilers.  However, it slows
+;; compilation down, and some compilers (*cough* Python *cough*) like
+;; to punt after a couple rounds of simplification.
+
 (defun simplify-indexing-expression (index env)
+  (when (typep index `(or atom
+                          (not (cons (member + *)))
+                          (cons t (cons atom (or null
+                                                 (cons atom null))))))
+    (return-from simplify-indexing-expression index))
   (let ((terms  (make-hash-table :test #'equal)) ; multiplier -> operands
         (noise '())
         (offset 0)
@@ -71,7 +99,7 @@
                       (incf offset (* *current-multiplier*
                                       (sb-int:constant-form-value term env))))
                      #-sbcl
-                     ((integerp term)
+                     ((numberp term)
                       (incf offset (* *current-multiplier* term)))
                      #-sbcl
                      ((constantp term env)
@@ -92,7 +120,7 @@
                           (setf literal (* literal
                                            (sb-int:constant-form-value term env))))
                          #-sbcl
-                         ((integerp term)
+                         ((numberp term)
                           (setf literal (* literal term)))
                          #-sbcl
                          ((constantp term env)
@@ -129,8 +157,8 @@
 (define-compiler-macro ref (array index &environment env)
   `(aref ,array ,(simplify-indexing-expression index env)))
 
-(define-compiler-macro (setf ref) (value array index &environment env)
-  (let ((_value (gensym "VALUE")))
-    `(let ((,_value ,value))
-       (setf (aref ,array ,(simplify-indexing-expression index env))
-             ,_value))))
+(define-compiler-macro %set (array index value &environment env)
+  `(setf (aref ,array ,(simplify-indexing-expression index env))
+         ,value))
+
+(defsetf ref %set)
