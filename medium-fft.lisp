66ef071d4563a6908d9eb7473b7c99cc8593837d
Paul Khuong <pvk@pvk.ca>
2011-12-27 20:21:07 -0500

    Take stride into account correctly

--- medium-fft.lisp
+++ medium-fft.lisp
@@ -20,17 +20,21 @@
               (declare (type complex-sample-array dst src twiddle)
                        (type index startd starts))
               ,(gen-fft/small half-size)))
+       ;; copy columns from src to tmp
        (loop for i of-type index below ,half-size by ,+blocking-factor+
              for j of-type index from 0 by ,(* +blocking-factor+ half-size)
              do (loop for count of-type index from ,half-size above 0
                       for j from ,startt
-                      for k of-type index from (+ i ,starts) by ,half-size
+                      for k of-type index from (+ (* ,strides i) ,starts)
+                        by (* ,strides ,half-size)
                       do (setf ,@(loop
                                    for block below +blocking-factor+
                                    append
                                    `((ref ,tmp (+ j ,(* block half-size)))
-                                     (ref ,src (* (+ k ,block) ,strides))))))
+                                     (ref ,src (+ k (* ,block ,strides)))))))
              do (progn
+                  ;; FFT columns in scratch space
+                  ;; write result to dst, in rows
                   ,@(loop
                       for block below +blocking-factor+
                       collect
@@ -85,59 +89,67 @@
          (size2 (/ size size1)))
     (assert (integerp size2))
     `(progn
-       (flet ((rec (startd starts)
+       (flet ((rec (dst src startd starts)
                 ,(gen-fft/small size2
-                                :dst dst :src tmp :twiddle twiddle
+                                :dst 'dst :src 'src :twiddle twiddle
                                 :startd 'startd
                                 :starts 'starts
                                 :strided strided)))
+         ;; copy columns to scratch
          (loop for i of-type index below ,size1 by +blocking-factor+
                for j of-type index from 0 by ,(* +blocking-factor+ size2)
                do (loop for count of-type index from ,size2 above 0
                         for j from ,startt
-                        for k of-type index from (+ i ,starts) by ,size1
+                        for k of-type index from (+ (* i ,strides) ,starts)
+                          by (* ,size1 ,strides)
                         do (setf ,@(loop
                                      for block below +blocking-factor+
                                      append `((ref ,tmp (+ j ,(* block size2)))
-                                              (ref ,src (* (+ k ,block) ,strides))))))
+                                              (ref ,src (+ k (* ,block ,strides)))))))
                do (progn
                     ,@(loop
                         for block below +blocking-factor+
-                        collect `(rec (+ j ,(* block size2) ,startd)
+                        collect `(rec ,dst ,tmp
+                                      (+ (* ,strided (+ j ,(* block size2)))
+                                         ,startd)
                                       (+ ,startt ,(* block size2))))
-                    (loop for i of-type index from (+ j ,startd) by ,strided
+                    (loop for i of-type index from (+ (* j ,strided) ,startd) by ,strided
                           for idx of-type index from j
                           for count below ,(* size2 +blocking-factor+)
                           do (setf (ref ,dst i)
                                    (* (ref ,dst i)
                                       (ref ,cooley-tukey idx)))))))
-       (flet ((rec (startd starts)
+       (flet ((rec (vec startd starts)
                 ,(gen-fft/small size1
-                                :dst tmp :src tmp :twiddle twiddle
+                                :dst 'vec :src 'vec :twiddle twiddle
                                 :startd 'startd
                                 :starts 'starts)))
          (loop for i of-type index below ,size2 by +blocking-factor+ do
            (loop for count from ,size1 above 0
                  for j of-type index from ,startt
-                 for k of-type index from (+ i ,startd) by (* ,size2 ,strided)
+                 for k of-type index from (+ (* i ,strided) ,startd)
+                   by (* ,size2 ,strided)
                  do (setf ,@(loop
                               for block below +blocking-factor+
                               append `((ref ,tmp (+ j ,(* block size1)))
-                                       (ref ,dst (+ k ,block))))))
+                                       (ref ,dst (+ k (* ,block ,strided)))))))
            do (progn
                 ,@(loop
                     for block below +blocking-factor+
-                    collect `(rec (+ ,startt ,(* +blocking-factor+ size1)
+                    collect `(rec ,tmp
+                                  (+ ,startt ,(* +blocking-factor+ size1)
                                      ,(* block size1))
                                   (+ ,startt ,(* block size1))))
                 (loop for count from ,size1 above 0
                       for j of-type index
                         from (+ ,startt ,(* +blocking-factor+ size1))
-                      for k of-type index from (+ i ,startd) by (* ,size2 ,strided)
+                      for k of-type index from (+ (* i ,strided) ,startd)
+                        by (* ,size2 ,strided)
                       do (setf
                           ,@(loop for block below +blocking-factor+
                                   append `((ref ,dst (+ k ,block))
-                                           (ref ,tmp (+ j ,(* block size1))))))))))
+                                           (ref ,tmp (+ j (* ,strided
+                                                             ,(* block size1)))))))))))
        dst)))
 
 (defun gen-fft/medium (size &rest args
